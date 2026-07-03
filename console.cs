10b659279d203919b2244954f8ba9a7c60de7109
BlueBill365 <paulkwadade365@gmail.com>
2026-07-03 22:52:55 +0000

    if condition understood clearly

--- console.cs
+++ console.cs
@@ -8,10 +8,28 @@ namespace Tuts
           {
 
                int number1, number2, result;
+               string answer;
+
               Console.WriteLine("----Calculator----");
-              Console.WriteLine("Please enter the first number: ");
+              Console.WriteLine("Enter the first number: ");
+             number1 = Convert.ToInt32(Console.ReadLine());
 
+             Console.WriteLine("Enter the second number: ");
+             number2 = Convert.ToInt32(Console.ReadLine());
 
+             Console.WriteLine("Enter your Operations (+, -, *, /): ");
+             answer = Console.ReadLine();
+
+             if (answer == "+")
+               {
+                    result = number1 + number2;
+                    Console.WriteLine("Answer: " + result);
+               }
+               else if (answer == "-")
+               {
+                    result = number1 - number2;
+                    Console.WriteLine("Answer: " + result);
+               }
              Console.ReadKey();
           }
      }
